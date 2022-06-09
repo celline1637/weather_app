cb181053c266edbd453419addcbc0c207b52b0ac
sunkyungnoh <cellin1637@gmail.com>
2022-06-09 14:17:34 +0900

    Feature : 도시 검색 기능 구현

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,9 +1,19 @@
+import React, { useRef } from 'react'
+import { useNavigateTo } from '../../hooks'
 import * as S from './style'
 
 const SearchBar = () => {
+  const { goTo } = useNavigateTo()
+  const searchRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    goTo(`/city/${searchRef.current?.value}`)
+  }
+
   return (
-    <S.Wrapper>
-      <S.Input type="text" placeholder="검색어를 입력하세요" />
+    <S.Wrapper onSubmit={handleSubmit}>
+      <S.Input ref={searchRef} type="text" placeholder="검색어를 입력하세요" />
     </S.Wrapper>
   )
 }
--- src/components/searchBar/style.ts
+++ src/components/searchBar/style.ts
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.form`
-  margin-right: auto;
+  width: 60vw;
+  margin-left: auto;
+  margin-right: 1rem;
+  @media ${({ theme }) => theme.tablet} {
+    width: 300px;
+    margin-right: auto;
+    margin-left: 1rem;
+  }
 `
 export const Input = styled.input`
-  width: 250px;
+  width: 100%;
   padding: 0.4rem 0.9rem;
   background-color: ${({ theme }) => theme.colors.grey100};
   border-radius: 25px;
